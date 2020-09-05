37456b0004b8f621b6240d5ade0879863cc20e7e
karanwadhwa <karanwadhwa0309@gmail.com>
2020-09-05 20:07:51 +0530

    Updates

---
+++ precache-manifest.f951921a11eca081e92b1935ee5f6c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15e47e10ae3741823062bf31b9db72e",
+    "url": "/ReactCountriesApi/index.html"
+  },
+  {
+    "revision": "58a474ef1a60fc14be9d",
+    "url": "/ReactCountriesApi/static/css/main.a1a7f45c.chunk.css"
+  },
+  {
+    "revision": "1067c1e22d3b93cbb873",
+    "url": "/ReactCountriesApi/static/js/2.7e9ed0db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactCountriesApi/static/js/2.7e9ed0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a474ef1a60fc14be9d",
+    "url": "/ReactCountriesApi/static/js/main.4fa47564.chunk.js"
+  },
+  {
+    "revision": "f81044808ce18e7123c7",
+    "url": "/ReactCountriesApi/static/js/runtime-main.6ff6534f.js"
+  }
+]);
